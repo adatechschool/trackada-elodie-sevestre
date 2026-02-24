e66a57bd6aeecbc0a7ef3751a381d12bdf099795
Elodie S <elodie.sevestre86@gmail.com>
2026-02-24 10:00:30 +0100

    maj etape 1

--- checktrack.js
+++ checktrack.js
@@ -6,15 +6,7 @@ console.log("hello elo y a de l'echo"); // le terminal affiche bien le message -
 
 // Le script (checktrack.js) doit : Fichier JSON  →  Script Node.js  →  Vérifications  →  Messages dans le terminal
 
-// Etape 1 : Lire un fichier JSON
-// Consigne : utiliser des fonctions disponibles de Node.js (PAS de framework)
-//  fonctions à utiliser : via des modules de Node.js
-//      module fs : manipuler le système de fichiers
-//                  readFileSync et existSync du module fs
-//      module path : manipuler des chemins de fichiers
-//                  join du module path
-//      module os : en savoir plus sur les capacités de l'ordinateur
-//                  homedir du module os
+// Etape 1 : Charger et parser en JSON le fichier track.json
 
 // Etape 1.1 : Import des modules
 
@@ -22,7 +14,7 @@ import { readFileSync, existsSync } from "fs"; // vérifier l'existence du fichi
 import { homedir } from "os";
 import { join } from "path";
 
-// vérifier l'existence du fichier JSON
+// Etape 1.2 : Vérifier l'existence du fichier JSON
 
 if (existsSync("track.json")) {
   console.log("file exists");
@@ -30,14 +22,13 @@ if (existsSync("track.json")) {
   console.log("file does'nt exist");
 }
 
-// lire le fichier JSON et le parser pour l'utiliser dans JS
+// Etape 1.3 : Lire le fichier JSON et le parser pour l'utiliser dans JS
 
 const data = readFileSync("track.json", "utf-8");
 const track = JSON.parse(data);
-
 // console.log(track);
 
-// Etape 1.2 : Vérifier les dossiers
+// Etape 2 : Vérifier que le dossier ada existe dans le dossier home (~)
 
 const homePath = homedir();
 console.log(homePath);
